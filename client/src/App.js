a6163703e2bd3f934baaaaf38b3677da2b87a07f
Ramsay Romero <Ramsay.Romero@gmail.com>
2021-03-05 13:30:06 -0800

    client setup with navbar

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path='/' exact>
+        <Navbar />
+      </Route>
+    </BrowserRouter>
+  );
+};
+
+export default App;
